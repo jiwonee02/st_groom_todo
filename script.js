8040d436115ac96f12ad283b6a22d9b1a03c86f8
jiwonee02 <nepthun@naver.com>
2024-09-14 15:51:42 +0900

    todo 구현

--- script.js
+++ script.js
@@ -1,96 +1,125 @@
 const list = document.getElementById("list");
-const createBtn = document.getElementById("create_btn");
+const createBtn = document.getElementById("create-btn");
 
-let todos = loadFromLocalStorage() || [];
+let todos = [];
 
-// 초기 렌더링
-displayTodos();
+createBtn.addEventListener("click", createNewTodo);
+function createNewTodo() {
+  const item = {
+    id: new Date().getTime(),
+    text: "",
+    complete: false,
+  };
 
-// 새로운 todo 추가
-createBtn.addEventListener("click", () => {
-  const newItem = { id: Date.now(), text: "", complete: false };
-  todos.unshift(newItem);
-  renderTodoItem(newItem, true);
-  saveToLocalStorage();
-});
+  todos.unshift(item);
 
-function renderTodoItem(item, focus = false) {
-  const itemEl = document.createElement("div");
-  itemEl.classList.add("item");
-  if (item.complete) itemEl.classList.add("complete");
+  const { itemEl, inputEl } = createTodoElement(item);
 
-  const checkbox = createCheckbox(item);
-  const inputEl = createTextInput(item, focus);
-  const actionsEl = createActions(item, itemEl);
-
-  itemEl.append(checkbox, inputEl, actionsEl);
   list.prepend(itemEl);
+
+  inputEl.removeAttribute("disabled");
+
+  inputEl.focus();
+
+  saveToLocalStorage();
 }
 
-function createCheckbox(item) {
+function createTodoElement(item) {
+  const itemEl = document.createElement("div");
+  itemEl.classList.add("item");
+
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
   checkbox.checked = item.complete;
-  checkbox.addEventListener("change", () => {
-    item.complete = checkbox.checked;
-    saveToLocalStorage();
-  });
-  return checkbox;
-}
 
-function createTextInput(item, focus) {
+  if (item.complete) {
+    itemEl.classList.add("complete");
+  }
+
   const inputEl = document.createElement("input");
   inputEl.type = "text";
   inputEl.value = item.text;
-  inputEl.disabled = !focus;
+  inputEl.setAttribute("disabled", "");
+
+  const actionsEl = document.createElement("div");
+  actionsEl.classList.add("actions");
+
+  const editBtnEl = document.createElement("button");
+  editBtnEl.classList.add("material-icons");
+  editBtnEl.innerText = "edit";
+
+  const removeBtnEl = document.createElement("button");
+  removeBtnEl.classList.add("material-icons", "remove-btn");
+  removeBtnEl.innerText = "remove_circle";
+
+  actionsEl.append(editBtnEl);
+  actionsEl.append(removeBtnEl);
+
+  itemEl.append(checkbox);
+  itemEl.append(inputEl);
+  itemEl.append(actionsEl);
+
+  checkbox.addEventListener("change", () => {
+    item.complete = checkbox.checked;
+
+    if (item.complete) {
+      itemEl.classList.add("complete");
+    } else {
+      itemEl.classList.remove("complete");
+    }
+
+    saveToLocalStorage();
+  });
 
   inputEl.addEventListener("input", () => {
     item.text = inputEl.value;
   });
 
   inputEl.addEventListener("blur", () => {
-    inputEl.disabled = true;
+    inputEl.setAttribute("disabled", "");
+
     saveToLocalStorage();
   });
 
-  if (focus) inputEl.focus();
-  return inputEl;
-}
-
-function createActions(item, itemEl) {
-  const actionsEl = document.createElement("div");
-  actionsEl.classList.add("actions");
-
-  const editBtn = document.createElement("button");
-  editBtn.classList.add("material-icons");
-  editBtn.innerText = "edit";
-  editBtn.addEventListener("click", () => {
-    const inputEl = itemEl.querySelector("input[type='text']");
-    inputEl.disabled = false;
+  editBtnEl.addEventListener("click", () => {
+    inputEl.removeAttribute("disabled");
     inputEl.focus();
   });
 
-  const removeBtn = document.createElement("button");
-  removeBtn.classList.add("material-icons", "remove-btn");
-  removeBtn.innerText = "remove_circle";
-  removeBtn.addEventListener("click", () => {
-    todos = todos.filter((t) => t.id !== item.id);
+  removeBtnEl.addEventListener("click", () => {
+    todos = todos.filter((t) => t.id != item.id);
     itemEl.remove();
+
     saveToLocalStorage();
   });
 
-  actionsEl.append(editBtn, removeBtn);
-  return actionsEl;
+  return { itemEl, inputEl, editBtnEl, removeBtnEl };
 }
 
 function displayTodos() {
-  todos.forEach((item) => renderTodoItem(item));
+  loadFromLocalStorage();
+
+  for (let i = 0; i < todos.length; i++) {
+    const item = todos[i];
+
+    const { itemEl } = createTodoElement(item);
+
+    list.append(itemEl);
+  }
 }
 
+displayTodos();
+
 function saveToLocalStorage() {
-  localStorage.setItem("my_todos", JSON.stringify(todos));
+  const data = JSON.stringify(todos);
+
+  localStorage.setItem("my_todos", data);
 }
 
 function loadFromLocalStorage() {
-  return JSON.parse(localStorage.getItem("my_todos"));
+  const data = localStorage.getItem("my_todos");
+
+  if (data) {
+    todos = JSON.parse(data);
+  }
 }
